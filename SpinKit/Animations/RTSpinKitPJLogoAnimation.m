c93b93af22b8d0d21e81545fef335783c15feee5
Pieter Jongsma <pieter.jongsma@gmail.com>
2015-01-26 19:50:41 +0100

    Rudimentary PJ logo animation.

--- SpinKit/Animations/RTSpinKitPJLogoAnimation.m
+++ SpinKit/Animations/RTSpinKitPJLogoAnimation.m
@@ -10,51 +10,99 @@
 
 @implementation RTSpinKitPJLogoAnimation
 
--(void)setupSpinKitAnimationInLayer:(CALayer*)layer withSize:(CGSize)size color:(UIColor*)color
+- (void)setupSpinKitAnimationInLayer:(CALayer*)layer withSize:(CGSize)size color:(UIColor*)color
 {
+    // Positions:
+    //
+    // - - - 1
+    // - 0 - 2
+    // - - - 3
+    // 7 6 5 4
+
     NSTimeInterval beginTime = CACurrentMediaTime();
 
-    CGFloat squareSize = size.width / 3;
+    CGFloat squareSize = size.width / 4;
 
-    for (NSInteger sum = 0; sum < 5; sum++)
-    {
-        for (NSInteger x = 0; x < 3; x++)
-        {
-            for (NSInteger y = 0; y < 3; y++)
-            {
-                if (x + y == sum)
-                {
-                    CALayer *square = [CALayer layer];
-                    square.frame = CGRectMake(x * squareSize, y * squareSize, squareSize, squareSize);
-                    square.backgroundColor = color.CGColor;
-                    square.transform = CATransform3DMakeScale(0.0, 0.0, 0.0);
+    for (NSInteger position = 0; position < 8; position++) {
+        NSInteger x = [self xForPosition:position];
+        NSInteger y = [self yForPosition:position];
 
-                    CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
-                    anim.removedOnCompletion = NO;
-                    anim.repeatCount = HUGE_VALF;
-                    anim.duration = 1.5;
-                    anim.beginTime = beginTime + (0.1 * sum);
-                    anim.keyTimes = @[@(0.0), @(0.4), @(0.6), @(1.0)];
+        CALayer *square = [CALayer layer];
+        square.frame = CGRectMake(x * squareSize, y * squareSize, squareSize, squareSize);
+        square.backgroundColor = color.CGColor;
+        square.transform = CATransform3DMakeScale(0.0, 0.0, 0.0);
 
-                    anim.timingFunctions = @[
-                                             [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
-                                             [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
-                                             [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
-                                             [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]
-                                             ];
+        CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+        anim.removedOnCompletion = NO;
+        anim.repeatCount = HUGE_VALF;
+        anim.duration = 1.5;
+        anim.beginTime = beginTime + (0.1 * position);
+        anim.keyTimes = @[@(0.0), @(0.4), @(0.6), @(1.0)];
 
-                    anim.values = @[
-                                    [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)],
-                                    [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
-                                    [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
-                                    [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)]
-                                    ];
-                    
-                    [layer addSublayer:square];
-                    [square addAnimation:anim forKey:@"spinkit-anim"];
-                }
-            }
-        }
+        anim.timingFunctions = @[
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]
+                                 ];
+
+        anim.values = @[
+                        [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)]
+                        ];
+
+        [layer addSublayer:square];
+        [square addAnimation:anim forKey:@"spinkit-anim"];
+    }
+}
+
+- (NSInteger)xForPosition:(NSInteger)position
+{
+    switch (position) {
+        case 7:
+            return 0;
+            break;
+        case 0:
+        case 6:
+            return 1;
+            break;
+        case 5:
+            return 2;
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            return 3;
+        default:
+            return -1;
+            break;
+    }
+}
+- (NSInteger)yForPosition:(NSInteger)position
+{
+    switch (position) {
+        case 1:
+            return 0;
+            break;
+        case 0:
+        case 2:
+            return 1;
+            break;
+        case 3:
+            return 2;
+            break;
+        case 7:
+        case 6:
+        case 5:
+        case 4:
+            return 3;
+            break;
+        default:
+            return -1;
+            break;
     }
 }
 
